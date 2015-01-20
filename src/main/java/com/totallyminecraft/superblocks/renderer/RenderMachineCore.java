0e106178792a3d663a00d1cfefe7944c168d8ffe
pooboy92 <mc2minecraftinfo@gmail.com>
2015-01-20 02:21:19 -0500

    Updated Machine Core model

---
+++ src/main/java/com/totallyminecraft/superblocks/renderer/RenderMachineCore.java
@@ -0,0 +1,37 @@
+package com.totallyminecraft.superblocks.renderer;
+
+import com.totallyminecraft.superblocks.lib.Constants;
+import com.totallyminecraft.superblocks.model.ModelMachineCore;
+import com.totallyminecraft.superblocks.tileEntity.TileEntityMachineCore;
+import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ResourceLocation;
+import org.lwjgl.opengl.GL11;
+
+
+public class RenderMachineCore extends TileEntitySpecialRenderer{
+
+    private static final ResourceLocation texture = new ResourceLocation(Constants.MODID + ":" + "textures/model/ModelMachineCore.png");
+
+    private ModelMachineCore model;
+
+    public RenderMachineCore(){
+        this.model = new ModelMachineCore();
+    }
+
+    @Override
+    public void renderTileEntityAt(TileEntity tileentity, double x, double y, double z, float f){
+        GL11.glPushMatrix();
+        GL11.glTranslatef((float)x + 0.5F, (float)y + 1.5625F, (float)z + 0.5F);
+        TileEntityMachineCore tile = (TileEntityMachineCore) tileentity;
+        GL11.glRotatef(90, 0.0F, 1.0F, 0.0F);
+            this.bindTexture(texture);
+
+            GL11.glPushMatrix();
+                this.model.renderModel(0.06525F);
+            GL11.glPopMatrix();
+
+        GL11.glPopMatrix();
+    }
+
+}
---
+++ src/main/java/com/totallyminecraft/superblocks/tileEntity/TileEntityMachineCore.java
@@ -0,0 +1,8 @@
+package com.totallyminecraft.superblocks.tileEntity;
+
+
+import net.minecraft.tileentity.TileEntity;
+
+public class TileEntityMachineCore extends TileEntity{
+
+}
